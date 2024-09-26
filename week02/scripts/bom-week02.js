a6e2b6510254723d0d0ea19bed9a562eb684600c
smarquis20 <shaunmarquis@gmail.com>
2024-09-25 22:00:45 -0500

    Click Button Functionality

--- week02/scripts/bom-week02.js
+++ week02/scripts/bom-week02.js
@@ -1,10 +1,28 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('ul');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+const list = document.querySelector('#list');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+        input.focus();
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+    } else {
+        input.focus();
+    }
+});
