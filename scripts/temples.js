d5736ffd38c777df50443b9578c73904b9dde117
smarquis20 <shaunmarquis@gmail.com>
2024-09-28 23:01:52 -0500

    updated hamburger menu options

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,6 +7,7 @@ const headerTitle = document.querySelector('header h1');
 const navLinks = document.querySelectorAll('nav ul li a');
 const mainTitle = document.querySelector('main h2');
 
+//This will hide the h1 title and open the hamburger menu
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('open');
     hambutton.classList.toggle('open');
@@ -17,6 +18,17 @@ hambutton.addEventListener('click', () => {
     }
 });
 
+//If the window is resized to the larger screen while the hamburger window is open this will restore the h1 title
+//and close out the hidden hamburger menu
+window.addEventListener('resize', () => {
+    if(window.innerWidth >= 700) {
+        headerTitle.style.display = "flex";
+        mainnav.classList.remove('open');
+        hambutton.classList.remove('open');
+    }
+});
+
+//This changes the t2 header to the name of the menu button you click on
 navLinks.forEach(link => {
     link.addEventListener('click', (event) => {
         event.preventDefault();
